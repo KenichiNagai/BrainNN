8b60d469fb15fad77d1888489179165cd7621da7
KenichiNagai <s181058@stn.nagaokaut.ac.jp>
2022-07-18 12:15:48 +0900

    pyauto

--- rex.py
+++ rex.py
@@ -1,2 +1,17 @@
-import pyautogui
+import time
+import pyautogui as pa
 
+i=0
+while(i<720):
+    pos = pa.position()
+    print(pos)
+    pa.click(x=576, y=1139)
+
+    # BSおす
+    # pa.press("backspace")
+    # A入力
+    #1秒まつ
+    time.sleep(10.0)
+    i += 1
+
+# 576.1139
