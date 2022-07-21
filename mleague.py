812d624c325b7adc41e6da913cc30c7e1f48c1c4
KenichiNagai <s181058@stn.nagaokaut.ac.jp>
2022-07-21 22:57:25 +0900

    Mリーグ

---
+++ mleague.py
@@ -0,0 +1,79 @@
+import pandas as pd
+from pyparsing import autoname_elements
+from sklearn.decomposition import PCA
+import seaborn as sns
+
+import matplotlib
+from matplotlib.font_manager import FontProperties
+font_path = "/usr/share/fonts/opentype/ipaexfont-gothic/ipaexg.ttf"
+font_prop = FontProperties(fname=font_path)
+matplotlib.rcParams["font.family"] = font_prop.get_name()
+import matplotlib.pyplot as plt
+
+
+def main():
+    df = pd.read_csv('mleague2021.csv', index_col=0, sep=',')
+    print('df')
+    print(df)
+
+    df.drop(['総局数'], axis=1, inplace=True)
+    df.drop(['1位'], axis=1, inplace=True)
+    df.drop(['2位'], axis=1, inplace=True)
+    df.drop(['3位'], axis=1, inplace=True)
+    df.drop(['4位'], axis=1, inplace=True)
+
+
+    label = df['Team']
+    df.drop(['Team'], axis=1, inplace=True)
+
+    x = df.iloc[:, 1:]  # 最初の列のbandgapを目的変数とする
+    autoscaled_x = (x - x.mean()) / x.std()  # オートスケーリング
+    print(autoscaled_x)
+
+    pca = PCA()  # PCA を行ったり PCA の結果を格納したりするための変数を、pca として宣言
+    pca.fit(autoscaled_x)  # PCA を実行
+
+    score = pd.DataFrame(pca.transform(autoscaled_x), index=autoscaled_x.index)
+    score['Team'] = label
+
+    print('score')
+    print(score)
+    score.to_csv('mleague_pca.csv', encoding='utf-8')
+
+    loadings = pd.DataFrame(pca.components_.T, index=autoscaled_x.columns)
+    print('loadings')
+    print(loadings)
+    loadings.to_csv('mleague_loadings.csv', encoding='utf-8')
+
+    contribution_ratios = pd.DataFrame(pca.explained_variance_ratio_)
+    print('contribution_ratios')
+    print(contribution_ratios)
+    contribution_ratios.to_csv('mleague_contribution_ratios.csv', encoding='utf-8')
+
+    groups = score.groupby('Team')
+    fig, ax = plt.subplots()
+    ax.margins(0.05) # Optional, just adds 5% padding to the autoscaling
+    for name, group in groups:
+        ax.plot(group[0], group[1], marker='o', linestyle='', ms=6, label=name)
+    ax.legend()
+    plt.savefig("mleague_scatter.png")
+    plt.show()
+
+    # 相関行列
+    correlation_coefficients = df.corr()  # 相関行列の計算
+
+    # 相関行列のヒートマップ (相関係数の値あり) 
+    plt.rcParams['font.size'] = 12
+    plt.figure(figsize=(12, 8))  # この段階で画像のサイズを指定する
+    sns.heatmap(correlation_coefficients, vmax=1, vmin=-1, cmap='seismic', square=True, annot=True, xticklabels=1, yticklabels=1)
+    plt.xlim([0, correlation_coefficients.shape[0]])
+    plt.savefig("mleague_heatmap.png")
+    plt.show()
+
+
+
+
+
+
+
+main()
---
+++ tsubone_resevoir/main.py
@@ -0,0 +1,71 @@
+# https://qiita.com/pokotsun/items/dd8eb48fadeee052110b
+# ゼロから作るReservoir Computing
+
+
+import numpy as np
+from input_generator import InputGenerator
+from reservoir_computing import ReservoirNetWork
+import matplotlib.pyplot as plt
+
+# T = np.pi * 16 
+# RATIO_TRAIN = 0.9
+# dt = np.pi * 0.01
+# AMPLITUDE = 0.9
+LEAK_RATE=0.9
+NUM_INPUT_NODES = 1
+NUM_RESERVOIR_NODES = 2000
+NUM_OUTPUT_NODES = 1
+# NUM_TIME_STEPS = int(T/dt)
+
+# example of activator
+def ReLU(x):
+    return np.maximum(0, x)
+
+def main():
+    # i_gen = InputGenerator(0, T, NUM_TIME_STEPS)
+    # data = i_gen.genetare_logistic()
+
+    x0 = [0.1]
+    x = np.array(x0)
+    print(x)
+    a = 3.9
+    for i in range(1101):
+        print(x[-1])
+        # print(a * x[-1] * (1- x[-1]))
+        x = np.append(x, a * x[-1] * (1.0- x[-1]))
+    data = x
+
+    print(data)
+
+
+
+    num_train = 1001
+    train_data = data[:num_train]
+
+    model = ReservoirNetWork(inputs=train_data,
+        num_input_nodes=NUM_INPUT_NODES, 
+        num_reservoir_nodes=NUM_RESERVOIR_NODES, 
+        num_output_nodes=NUM_OUTPUT_NODES, 
+        leak_rate=LEAK_RATE)
+
+    model.train() # 訓練
+    train_result = model.get_train_result() # 訓練の結果を取得
+
+    num_predict = int(len(data[num_train:]))
+    predict_result = model.predict(num_predict)
+
+    t = np.arange(1102)
+
+    ## plot
+    plt.plot(t, data, label="inputs")
+    plt.plot(t[:num_train], train_result, label="trained")
+    plt.plot(t[num_train:], predict_result, label="predicted")
+    plt.axvline(x=1000, label="end of train", color="green") # border of train and prediction
+    plt.legend()
+    plt.title("ESN - Logistic Prediction")
+    plt.xlabel("step")
+    plt.ylabel("x_n")
+    plt.show()
+
+if __name__=="__main__":
+    main()
